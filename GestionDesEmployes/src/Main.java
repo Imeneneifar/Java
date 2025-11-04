05b9e1a451ab5d1aac7fad92616226621286b29d
imen <imen.neifar@esprit.tn>
2025-11-04 12:11:28 +0100

    prosit8

---
+++ GestionDesEmployes/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Employe em1 = new Employe(1, "imen", "neifar", "IT", 3);
+
+        System.out.println(em1);
+
+
+
+    }
+}
